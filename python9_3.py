564a25977d68da96ede6e3b6c7897db88fc68031
BogdanNV <comazo25@gmail.com>
2022-10-07 18:38:15 +0300

    relase v1

---
+++ python9_3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+nd_arr = np.array([
+               [12, 45, 78],
+               [34, 56, 13],
+               [12, 98, 76]
+               ])
+
+arr = np.array([345234, 876362.12, 0, -1000, 99999999], dtype=np.float32)
+print(arr)
